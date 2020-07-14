c121a45428085e4d4827ff0f3b99bf2ebab900f5
KZTN <kakaiocesar2@gmail.com>
2020-07-14 12:47:24 -0300

    :construction: Creeated Panel component
    all changes are in progress

---
+++ src/components/Panel/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import PanelSideBar from "../PanelSideBar";
+import "./styles.scss";
+export default function Panel() {
+  return (
+    <section id="panel">
+      <PanelSideBar />
+    </section>
+  );
+}
