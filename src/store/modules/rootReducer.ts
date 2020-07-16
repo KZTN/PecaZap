b8d7b15cbf5ba6bad93bb4973f3668e4b8fa3144
KZTN <kakaiocesar2@gmail.com>
2020-07-16 11:25:10 -0300

    :sparkles: Integrated Chats module to root modules
    all changes are cuntional

--- src/store/modules/rootReducer.ts
+++ src/store/modules/rootReducer.ts
@@ -3,5 +3,6 @@ import { combineReducers } from "redux";
 import Customers from "./Customers";
 import User from "./User";
 import Contacts from "./Contacts";
+import Chats from "./Chats";
 
-export default combineReducers({ Customers, User, Contacts });
+export default combineReducers({ Customers, User, Contacts, Chats });
--- src/store/modules/rootSaga.ts
+++ src/store/modules/rootSaga.ts
@@ -3,7 +3,8 @@ import { all } from "redux-saga/effects";
 import Customers from "./Customers/sagas";
 import User from "./User/sagas";
 import Contacts from "./Contacts/sagas";
+import Chats from "./Chats/sagas";
 
 export default function* rootSaga() {
-  return yield all([Customers, User, Contacts]);
+  return yield all([Customers, User, Contacts, Chats]);
 }
