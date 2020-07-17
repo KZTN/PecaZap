8a231e2964e5adc6777fdeb46f599084c6514135
KZTN <kakaiocesar2@gmail.com>
2020-07-17 11:47:57 -0300

    :lipstick: Updated styles from MailList component
    all changes are functional

--- src/components/MailList/index.tsx
+++ src/components/MailList/index.tsx
@@ -8,6 +8,8 @@ import { ChatsSelectMail } from "../../store/modules/Chats/actions";
 
 import Notification from "../Notification";
 
+import arrowRight from "../../assets/svgs/arrow_right_icon.svg";
+
 import "./styles.scss";
 
 export default function MailList() {
@@ -38,31 +40,44 @@ export default function MailList() {
         </thead>
         <tbody>
           {listMails.map((mail: Chat) => (
-            <tr key={mail.id} onClick={() => handleClick(mail.id)}>
-              <td>
-                <span>{mail.subject}</span>
-              </td>
-              <td>
-                <span>{moment.unix(mail.start).format("DD/MM/YYYY")}</span>
-              </td>
-              <td>
-                <span>
-                  {moment
-                    .unix(mail.messages[mail.messages.length - 1].timestamp)
-                    .format("DD/MM/YYYY")}
-                </span>
-              </td>
-              <td>
-                <div className="notification">
-                  {" "}
-                  {mail.messages[mail.messages.length - 1].seen === false ? (
-                    <Notification notifications={1} />
-                  ) : (
-                    ""
-                  )}
-                </div>
-              </td>
-            </tr>
+            <>
+              <tr
+                key={mail.id}
+                onClick={() => handleClick(mail.id)}
+                className={
+                  mail.messages[mail.messages.length - 1].seen === false
+                    ? "special"
+                    : ""
+                }
+              >
+                <td className={"special-left-td"}>
+                  <span>{mail.subject}</span>
+                </td>
+                <td>
+                  <span>{moment.unix(mail.start).format("DD/MM/YYYY")}</span>
+                </td>
+                <td>
+                  <span>
+                    {moment
+                      .unix(mail.messages[mail.messages.length - 1].timestamp)
+                      .format("DD/MM/YYYY")}
+                  </span>
+                </td>
+                <td className={"special-right-td"}>
+                  <div className="notification">
+                    {mail.messages[mail.messages.length - 1].seen === false ? (
+                      <>
+                        <Notification notifications={1} />
+                        <img src={arrowRight} alt="" />
+                      </>
+                    ) : (
+                      ""
+                    )}
+                  </div>
+                </td>
+              </tr>
+              <div className="blank-space" />
+            </>
           ))}
         </tbody>
       </table>
