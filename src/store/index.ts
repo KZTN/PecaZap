c4912a9bfb0df62d4c743b6a6bd19779119d8709
KZTN <kakaiocesar2@gmail.com>
2020-07-20 20:05:23 -0300

    :zap: Added structure to console status of redux-saga
    Using reactotron components

--- src/store/index.ts
+++ src/store/index.ts
@@ -3,7 +3,11 @@ import rootReducer from "./modules/rootReducer";
 import rootSaga from "./modules/rootSaga";
 import createSagaMiddleware from "redux-saga";
 
-const sagaMiddleware = createSagaMiddleware();
+const sagaMonitor =
+    process.env.NODE_ENV === 'development'
+        ? console.tron.createSagaMonitor()
+        : null;
+const sagaMiddleware = createSagaMiddleware({sagaMonitor});
 
 const store: Store = createStore(rootReducer, applyMiddleware(sagaMiddleware));
 
